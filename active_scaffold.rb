1034b8fdd943ad01a8b85c25433aa79753c63245
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 22:20:44 -0700

    wip on active-scaffold

--- active_scaffold.rb
+++ active_scaffold.rb
@@ -1,6 +1,8 @@
-plugin 'active_scaffold',   :git => 'http://github.com/vhochstein/active_scaffold.git', :submodule => true
-plugin 'verification',      :git => 'github.com/rails/verification.git', :submodule => true
+#adds active_scaffold plugin and dependents as submodules
+plugin 'active_scaffold',   :git => 'git://github.com/vhochstein/active_scaffold.git', :submodule => true
+
+plugin 'verification',      :git => 'git://github.com/rails/verification.git', :submodule => true
 
 #for embedded scaffolds
-plugin 'render_component',  :git => 'http://github.com/vhochstein/render_component.git', :submodule => true
+plugin 'render_component',  :git => 'git://github.com/vhochstein/render_component.git', :submodule => true
 
