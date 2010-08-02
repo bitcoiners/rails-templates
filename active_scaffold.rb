419cf66f67cc54cc06051fc3b6ece8848acba9ec
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 23:38:13 -0700

    added replacement for default application layout with active_scaffold includes added conditional foo scaffold

--- active_scaffold.rb
+++ active_scaffold.rb
@@ -4,3 +4,26 @@ plugin 'verification',      :git => 'git://github.com/rails/verification.git', :
 #for embedded scaffolds
 plugin 'render_component',  :git => 'git://github.com/vhochstein/render_component.git', :submodule => true
 
+file 'app/views/layouts/application.html.erb', 
+%q{<!DOCTYPE html>
+<html>
+<head>
+  <title>FIXME</title>
+  <%= stylesheet_link_tag :all %>
+  <%= javascript_include_tag :defaults %>
+  <%= active_scaffold_includes %>
+  <%= csrf_meta_tag %>
+</head>
+<body>
+
+<%= yield %>
+
+</body>
+</html>
+}
+
+
+if yes?("Do you want to generate a foo active_scaffold?")
+  generate active_scaffold foo bar:string
+  rake "db:migrate"
+end
