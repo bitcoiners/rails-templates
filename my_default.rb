ef9897b85501bbdb27d71f07659c4e27ae605591
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 20:30:23 -0700

    updated default to user rails 3 branch

--- my_default.rb
+++ my_default.rb
@@ -1,2 +1,12 @@
-#replaces prototype with jquery
-apply "http://github.com/slavix/rails-templates/raw/master/jquery.rb"
+#init git for the app
+apply "http://github.com/slavix/rails-templates/raw/rails3/newgit.rb"
+
+#replace prototype with jquery
+apply "http://github.com/slavix/rails-templates/raw/rails3/jquery.rb"
+
+
+#adds rspec
+apply "http://github.com/slavix/rails-templates/raw/rails3/rspec.rb"
+
+#commits all to git
+apply "http://github.com/slavix/rails-templates/raw/rails3/git-commit.rb"
