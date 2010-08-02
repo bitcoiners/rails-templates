4c1f8918561efb84746daab959552db4d0729daa
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 22:03:19 -0700

    updated rspec

--- rspec.rb
+++ rspec.rb
@@ -1,6 +1,4 @@
 #adds rspec gems and generates rspec install
-group :test do
-  gem "rspec", "2.0.0.beta.19"
-  gem "rspec-rails", "2.0.0.beta.19"
-end
+gem "rspec", "2.0.0.beta.19", :group => :test
+gem "rspec-rails", "2.0.0.beta.19", :group => :test
 generate("rspec:install")
