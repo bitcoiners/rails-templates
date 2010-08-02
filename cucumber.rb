885199de51ef86165735e0a9e2c51008dd70e1c6
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 20:55:27 -0700

    added cucumber gems and install

---
+++ cucumber.rb
@@ -0,0 +1,4 @@
+#adds cucumber gems and generates install
+gem 'cucumber-rails'
+gem 'capybara' #webrat alternative which aims to support all browser simulators 
+generate("cucumber:install")
--- rspec.rb
+++ rspec.rb
@@ -1,3 +1,4 @@
+#adds rspec gems and generates rspec install
 gem "rspec", "2.0.0.beta.19"
 gem "rspec-rails", "2.0.0.beta.19"
 generate("rspec:install")
