cbe33b2554a49aef352e6fab1d080ccf341a8b5f
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 20:01:46 -0700

    installs rspec and rspec-rails plugins as git submodules

---
+++ rspec.rb
@@ -0,0 +1,3 @@
+# Install submoduled rspec plugins
+plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git', :submodule => true
+plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git', :submodule => true
