ca548e7ed4bdd5cbe3318bf7547265c5dcd0e7f7
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 22:13:00 -0700

    added active_scaffold template

---
+++ active_scaffold.rb
@@ -0,0 +1,6 @@
+plugin 'active_scaffold',   :git => 'http://github.com/vhochstein/active_scaffold.git', :submodule => true
+plugin 'verification',      :git => 'github.com/rails/verification.git', :submodule => true
+
+#for embedded scaffolds
+plugin 'render_component',  :git => 'http://github.com/vhochstein/render_component.git', :submodule => true
+
