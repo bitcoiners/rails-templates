120f8699d79d5a8c3b4aa3fd022ef19ca94a6084
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 21:50:57 -0700

    misc

--- slavix_default.rb
+++ slavix_default.rb
@@ -2,11 +2,13 @@
 apply "http://github.com/slavix/rails-templates/raw/rails3/newgit.rb"
 
 #replace prototype with jquery
-#apply "http://github.com/slavix/rails-templates/raw/rails3/jquery.rb"
-
+apply "http://github.com/slavix/rails-templates/raw/rails3/jquery.rb"
 
 #adds rspec
 apply "http://github.com/slavix/rails-templates/raw/rails3/rspec.rb"
+
+#adds cucumber
+apply "http://github.com/slavix/rails-templates/raw/rails3/cucumber.rb"
 
 #commits all to git
 apply "http://github.com/slavix/rails-templates/raw/rails3/git-commit.rb"
