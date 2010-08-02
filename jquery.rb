3b2a3dd38775381f535c56d7fc3f21079d58d612
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-01 21:05:11 -0700

    changed jquery.rb to use :defaults only changed jquery.rb to load latest jquery 1.4+ from google

--- jquery.rb
+++ jquery.rb
@@ -11,39 +11,15 @@ inside('public/javascripts') do
   run "rm -rf controls.js dragdrop.js effects.js prototype.js rails.js"
 end
 
-# Downloading latest jQuery.min
-get "http://code.jquery.com/jquery-latest.min.js", "public/javascripts/jquery.js"
-
 # Downloading latest jQuery drivers
 get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 
 # Overriding default expansion
-if yes?("Override :defaults and setup :jquery expansion?")
-  initializer 'jquery.rb', <<-CODE
-  # Switch the javascript_include_tag :defaults to
-  # use jQuery instead of the default prototype helpers.
-  # Also setup a :jquery expansion, just for good measure.
-  # Written by: Logan Leger, logan@loganleger.com
-  # http://github.com/lleger/Rails-3-jQuery
+initializer 'jquery.rb', <<-CODE
+# Switch the javascript_include_tag :defaults to
+# use jQuery instead of the default prototype helpers.
+# Written by: Logan Leger, logan@loganleger.com
+# http://github.com/lleger/Rails-3-jQuery
 
-  ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery', 'rails']
-  Rails.application.config.action_view.javascript_expansions[:defaults] = ['jquery', 'rails']
-  CODE
-elsif yes?("Override :defaults only?")
-  initializer 'jquery.rb', <<-CODE
-  # Switch the javascript_include_tag :defaults to
-  # use jQuery instead of the default prototype helpers.
-  # Written by: Logan Leger, logan@loganleger.com
-  # http://github.com/lleger/Rails-3-jQuery
-
-  Rails.application.config.action_view.javascript_expansions[:defaults] = ['jquery', 'rails']
-  CODE
-elsif yes?("Setup :jquery expansion only?")
-  initializer 'jquery.rb', <<-CODE
-  # Setup a :jquery expansion, just for good measure.
-  # Written by: Logan Leger, logan@loganleger.com
-  # http://github.com/lleger/Rails-3-jQuery
-
-  ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery', 'rails']
-  CODE
-end
+Rails.application.config.action_view.javascript_expansions[:defaults] = ['http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min', 'rails']
+CODE
