c339fafd30b41de81eb8444e8044529cee24143c
Slava Mikerin <mikerin.slava@gmail.com>
2010-08-02 21:23:11 -0700

    fix for rspec

--- rspec.rb
+++ rspec.rb
@@ -1,9 +1,13 @@
+# rspec.rb
+# from Slava Mikerin
+
 #adds rspec gems and generates rspec install
 gem "rspec", "2.0.0.beta.19", :group => :test
 gem "rspec-rails", "2.0.0.beta.19", :group => :test
 generate("rspec:install")
 
-
-inject_into_file "config/application.rb", 
-                  "g.test_framework :rspec, :fixture => true, :views => false\n", 
-                  :after => "class Application < Rails::Application\n"
+inject_into_file "config/application.rb", :after => "class Application < Rails::Application\n" do
+  "\n\s\s\s\sconfig.generators do |g|
+    \tg.test_framework :rspec, :fixture => true, :views => true
+  \s\send\n\n"
+end
